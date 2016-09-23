5a32a4d4cc8bfe69819c9ff45a431dda7b3256ea
solemdal <temarual@gmail.com>
2016-09-23 15:21:53 +0000

    исправлен баг

--- wallie.py
+++ wallie.py
@@ -10,11 +10,11 @@ def get_wallie_action():
         'проверяет чью-то задачу',
         'проставляет ссылки в энциклопедии',
         'вычитывает статью',
-        'ищет стажировки для студентов',
+        'ищет стажировки для студентов'
     ]
     return 'Валли ' + random.choice(possible_actions)
 
 
 if __name__ == '__main__':
     action = get_wallie_action()
-    print action
+    print (action)
